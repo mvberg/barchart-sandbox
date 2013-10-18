cb4e2608d54e124d614e940e636ac1bfef99916e
mvberg <mvberg@gmail.com>
2013-10-18 17:07:47 -0500

    Added example with ExpandedListView and nested ViewPager

---
+++ src/com/barchart/tester/ExpandableListAdapter.java
@@ -0,0 +1,172 @@
+package com.barchart.tester;
+
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.barchart.tester.pager.InfinitePagerAdapter;
+import com.barchart.tester.pager.PagerContainer;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+	private Activity context;
+	private Map<String, List<String>> symbolCollection;
+	private List<String> symbols;
+
+	public ExpandableListAdapter(Activity context, List<String> symbols,
+			Map<String, List<String>> symbolCollection) {
+		this.context = context;
+		this.symbolCollection = symbolCollection;
+		this.symbols = symbols;
+	}
+
+	public Object getChild(int groupPosition, int childPosition) {
+		return symbolCollection.get(symbols.get(groupPosition)).get(
+				childPosition);
+	}
+
+	public long getChildId(int groupPosition, int childPosition) {
+		return childPosition;
+	}
+
+	public View getChildView(final int groupPosition, final int childPosition,
+			boolean isLastChild, View convertView, ViewGroup parent) {
+
+		LayoutInflater inflater = context.getLayoutInflater();
+
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.item_pager, null);
+			configurePager(convertView);
+		}
+
+		return convertView;
+	}
+
+	public int getChildrenCount(int groupPosition) {
+		return symbolCollection.get(symbols.get(groupPosition)).size();
+	}
+
+	public Object getGroup(int groupPosition) {
+		return symbols.get(groupPosition);
+	}
+
+	public int getGroupCount() {
+		return symbols.size();
+	}
+
+	public long getGroupId(int groupPosition) {
+		return groupPosition;
+	}
+
+	public View getGroupView(int groupPosition, boolean isExpanded,
+			View convertView, ViewGroup parent) {
+
+		String laptopName = (String) getGroup(groupPosition);
+		if (convertView == null) {
+			LayoutInflater infalInflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = infalInflater.inflate(R.layout.item_symbol, null);
+		}
+		TextView item = (TextView) convertView.findViewById(R.id.textView1);
+		item.setTypeface(null, Typeface.BOLD);
+		item.setText(laptopName);
+		return convertView;
+	}
+
+	public boolean hasStableIds() {
+		return false;
+	}
+
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		return false;
+	}
+
+	private void configurePager(final View view) {
+
+		PagerContainer container = (PagerContainer) view
+				.findViewById(R.id.pager_container);
+
+		ViewPager pager = container.getViewPager();
+		MyPagerAdapter adapter = new MyPagerAdapter();
+
+		// Necessary or the pager will only have one extra page to show
+		// make this at least however many pages you can see
+		pager.setOffscreenPageLimit(adapter.getCount());
+
+		// A little space between pages
+		pager.setPageMargin(15);
+
+		// wrap pager to provide infinite paging with wrap-around
+		InfinitePagerAdapter wrappedAdapter = new InfinitePagerAdapter(adapter);
+
+		// If hardware acceleration is enabled, you should also remove
+		// clipping on the pager for its children.
+		pager.setClipChildren(false);
+
+		pager.setOnTouchListener(mSuppressInterceptListener);
+
+		pager.setAdapter(wrappedAdapter);
+
+	}
+
+	private OnTouchListener mSuppressInterceptListener = new OnTouchListener() {
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			if (event.getAction() == MotionEvent.ACTION_DOWN
+					&& v instanceof ViewGroup) {
+				((ViewGroup) v).requestDisallowInterceptTouchEvent(true);
+			}
+			return false;
+		}
+	};
+
+	private class MyPagerAdapter extends PagerAdapter {
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+
+			final TextView view = (TextView) context.getLayoutInflater()
+					.inflate(R.layout.sample_view, null);
+
+			view.setText("Item " + position);
+
+			container.addView(view);
+
+			view.setBackgroundColor(0xFFFF0000);
+
+			// keep reference to your views here
+			//
+
+			return view;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			container.removeView((View) object);
+		}
+
+		@Override
+		public int getCount() {
+			return 12;
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object) {
+			return (view == object);
+		}
+	}
+
+}
--- src/com/barchart/tester/MainActivity.java
+++ src/com/barchart/tester/MainActivity.java
@@ -1,16 +1,14 @@
 package com.barchart.tester;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.barchart.tester.pager.InfinitePagerAdapter;
-import com.barchart.tester.pager.PagerContainer;
+import android.widget.ExpandableListView;
 
 /**
  * Demo application showing how to mimic "Gallery" like behavior.
@@ -20,36 +18,43 @@ import com.barchart.tester.pager.PagerContainer;
  */
 public class MainActivity extends Activity {
 
-	private PagerContainer container;
-	private ViewPager pager;
-	private PagerAdapter adapter;
-	private PagerAdapter wrappedAdapter;
+	private ExpandableListView symbol_list;
+
+	List<String> groupList;
+	List<String> childList;
+	Map<String, List<String>> symbolCollection = new HashMap<String, List<String>>();
+	ExpandableListView expListView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		container = (PagerContainer) findViewById(R.id.pager_container);
+		symbol_list = (ExpandableListView) findViewById(R.id.symbol_list);
 
-		pager = container.getViewPager();
-		adapter = new MyPagerAdapter();
+		groupList = new ArrayList<String>();
 
-		// Necessary or the pager will only have one extra page to show
-		// make this at least however many pages you can see
-		pager.setOffscreenPageLimit(adapter.getCount());
+		groupList.add("MSFT");
+		groupList.add("YHOO");
+		groupList.add("GOOG");
+		groupList.add("NEM");
+		groupList.add("EXC");
+		groupList.add("IBM");
 
-		// A little space between pages
-		pager.setPageMargin(15);
+		childList = new ArrayList<String>();
+		childList.add("single_row_tabber");
 
-		// wrap pager to provide infinite paging with wrap-around
-		wrappedAdapter = new InfinitePagerAdapter(adapter);
+		symbolCollection.put("MSFT", childList);
+		symbolCollection.put("YHOO", childList);
+		symbolCollection.put("GOOG", childList);
+		symbolCollection.put("NEM", childList);
+		symbolCollection.put("EXC", childList);
+		symbolCollection.put("IBM", childList);
 
-		// If hardware acceleration is enabled, you should also remove
-		// clipping on the pager for its children.
-		pager.setClipChildren(false);
+		final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(
+				this, groupList, symbolCollection);
 
-		pager.setAdapter(wrappedAdapter);
+		symbol_list.setAdapter(expListAdapter);
 
 	}
 
@@ -57,40 +62,6 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-
-	private class MyPagerAdapter extends PagerAdapter {
-
-		@Override
-		public Object instantiateItem(ViewGroup container, int position) {
-
-			final TextView view = (TextView) getLayoutInflater().inflate(
-					R.layout.sample_view, null);
-
-			view.setText("Item " + position);
-
-			container.addView(view);
-
-			// keep reference to your views here
-			//
-
-			return view;
-		}
-
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) {
-			container.removeView((View) object);
-		}
-
-		@Override
-		public int getCount() {
-			return 12;
-		}
-
-		@Override
-		public boolean isViewFromObject(View view, Object object) {
-			return (view == object);
-		}
 	}
 
 }
